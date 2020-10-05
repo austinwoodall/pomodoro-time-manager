6591439c276cfd384d7a796ef7899f7647a62097
Austin Woodall <austin.woodall72@gmail.com>
2020-10-05 02:20:53 -0500

    Home screen

--- screens/Home/index.tsx
+++ screens/Home/index.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Text } from '@ui-kitten/components';
+import { Image, View } from 'react-native';
+import { Layout, Text } from '@ui-kitten/components';
+import EmptyTaskSVG from '../../components/EmptyTaskSVG';
 
 function Home() {
 	return (
-		<View>
-			<Text>Home</Text>
-		</View>
+		<Layout style={{ flex: 1, paddingHorizontal: 20 }}>
+			<View style={{ height: 200, justifyContent: 'center', alignItems }}>
+				<Text
+					style={{ color: '#fff', fontSize: 30, fontWeight: 'bold' }}>
+					Task
+				</Text>
+				<View
+					style={{
+						alignItems: 'center',
+						justifyContent: 'center',
+					}}>
+					<EmptyTaskSVG style={{ height: 10, width: 100 }} />
+				</View>
+			</View>
+		</Layout>
 	);
 }
 
