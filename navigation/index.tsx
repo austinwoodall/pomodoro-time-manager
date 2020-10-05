09f540830f424dc0a76583b8962424989d0a7c60
Austin Woodall <austin.woodall72@gmail.com>
2020-10-05 02:21:12 -0500

    bottom tabs navigaiton

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -7,6 +7,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Timer from '../screens/Timer';
 import Home from '../screens/Home';
+import { BottomNavigationComponent } from '../components/BottomTabBar';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
@@ -15,11 +16,24 @@ const AppNavigator = () => {
 	return (
 		<NavigationContainer>
 			<Stack.Navigator>
-				<Stack.Screen name={'Task'} component={Home} />
-				<Stack.Screen name={'Timer'} component={Timer} />
+				<Stack.Screen
+					options={{ headerShown: false }}
+					name={'Task'}
+					component={ApplicationTabs}
+				/>
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
 };
+
+function ApplicationTabs() {
+	return (
+		<Tab.Navigator
+			tabBar={(props) => <BottomNavigationComponent {...props} />}>
+			<Tab.Screen name="Home Screen" component={Home} />
+			<Tab.Screen name={'Timer'} component={Timer} />
+		</Tab.Navigator>
+	);
+}
 
 export default AppNavigator;
